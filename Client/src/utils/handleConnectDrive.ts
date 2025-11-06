0764a1164889605284f0a4fc24e25460d54a7ead
Rhythmdoshi31 <rhythmdoshi04@gmail.com>
2025-11-06 11:24:22 +0530

    Seperated the 6 dots & 3 dots modals from Files.tsx into components to reuse later + added PATCH to CORS

--- Client/src/utils/handleConnectDrive.ts
+++ Client/src/utils/handleConnectDrive.ts
@@ -1,9 +1,8 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
 const handleConnectDrive = (): void => {
-  window.location.href = `${API_URL}/vip-list`;
+  // window.location.href = `${API_URL}/vip-list`;
+  window.location.href = "http://localhost:3000/auth/google";
 };
 
 export default handleConnectDrive;
-
-// window.location.href = "http://localhost:3000/auth/google";
