17ac8f8d3b82da1566a2210c175fcffcb526571f
Rhythmdoshi31 <rhythmdoshi04@gmail.com>
2026-01-17 00:27:33 +0530

    fixess

--- Client/src/utils/handleConnectDrive.ts
+++ Client/src/utils/handleConnectDrive.ts
@@ -1,8 +1,9 @@
 // const API_URL = import.meta.env.VITE_API_URL;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const handleConnectDrive = (): void => {
   // window.location.href = `${API_URL}/vip-list`;
-  window.location.href = "http://localhost:3000/auth/google";
+  window.location.href = `http://${BACKEND_URL}/auth/google`;
 };
 
 export default handleConnectDrive;
