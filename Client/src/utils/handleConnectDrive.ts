0d41ee45c7db90ebfd1c9c4009fc87abe6544e20
Rhythmdoshi31 <rhythmdoshi04@gmail.com>
2026-01-17 00:20:35 +0530

    fixes

--- Client/src/utils/handleConnectDrive.ts
+++ Client/src/utils/handleConnectDrive.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = import.meta.env.VITE_API_URL;
 
 const handleConnectDrive = (): void => {
   // window.location.href = `${API_URL}/vip-list`;
